946b5beb8c9b5e570fb207a240c311010d0aade0
Andrea <pavan97@hotmail.it>
2021-03-12 14:21:00 +0000

    added more instrument params

--- build/lib/instrument.js
+++ build/lib/instrument.js
@@ -6,15 +6,27 @@
 
   // GET /instruments/:accountId/candles
   exports.candles = function(req = {}) {
-    var granularity, id, includeFirst, route;
+    var count, from, granularity, id, includeFirst, price, route, to;
     validate(req, ['id']);
-    ({id, granularity, includeFirst} = req);
+    ({id, granularity, includeFirst, price, count, from, to} = req);
     route = `instruments/${id}/candles?`;
+    if (price) {
+      route = `${route}price=${price}&`;
+    }
     if (granularity) {
       route = `${route}granularity=${granularity}&`;
     }
     if (includeFirst) {
       route = `${route}includeFirst=${includeFirst}&`;
+    }
+    if (count) {
+      route = `${route}count=${count}&`;
+    }
+    if (from) {
+      route = `${route}from=${from}&`;
+    }
+    if (to) {
+      route = `${route}to=${to}&`;
     }
     return this.request(req, route, false);
   };
--- lib/instrument.coffee
+++ lib/instrument.coffee
@@ -3,10 +3,14 @@
 # GET /instruments/:accountId/candles
 exports.candles = (req = {}) ->
   validate req, ['id']
-  { id, granularity, includeFirst } = req
+  { id, granularity, includeFirst, price, count, from, to } = req
 
   route = "instruments/#{id}/candles?"
+  route = "#{route}price=#{price}&" if price
   route = "#{route}granularity=#{granularity}&" if granularity
   route = "#{route}includeFirst=#{includeFirst}&" if includeFirst
+  route = "#{route}count=#{count}&" if count
+  route = "#{route}from=#{from}&" if from
+  route = "#{route}to=#{to}&" if to
 
   return @request req, route, false
